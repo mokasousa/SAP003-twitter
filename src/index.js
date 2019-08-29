c0631380e9f09f971f44e12636d24fc7e3c80b67
mokasousa <moka.sousa@gmail.com>
2019-08-29 16:34:32 -0300

    local storage is working

--- src/index.js
+++ src/index.js
@@ -1,13 +1,43 @@
-let arrTweets = [];
+let input, arrTweets;
+
 document.getElementById("new-tweet").onsubmit = function() {
-  let input = document.getElementById("input-tweet").value;
+  input = document.getElementById("input-tweet").value;
+  let restoredTweets = JSON.parse(localStorage.getItem("arrTweets"));
+  if (!restoredTweets){
+    localStorage.setItem("arrTweets", 0);
+    arrTweets = [];
+  } else {
+    arrTweets= JSON.parse(localStorage.getItem("arrTweets"));
+  }
   arrTweets.push(input);
   console.log(arrTweets);
+  localStorage.setItem("arrTweets", JSON.stringify(arrTweets));
   let lastInput = arrTweets[arrTweets.length - 1]
-  let listItem = document.createElement("li");
-  let tweetInput = document.createTextNode(lastInput);
-  listItem.appendChild(tweetInput);
-  document.getElementById("tweets").appendChild(listItem);
+  printTweet(lastInput);
   document.getElementById("input-tweet").value = "";
   return false;
 };
+
+document.addEventListener("DOMContentLoaded", () => {
+  arrTweets = JSON.parse(localStorage.getItem("arrTweets"));
+  if (arrTweets) {
+    arrTweets.forEach((t) => {printTweet(t);});
+  }
+  let submitOff = document.getElementById("button-tweet").disabled;
+  document.getElementById("button-tweet").disabled = true;
+  document.getElementById("input-tweet").onkeyup = function() {
+    if(document.getElementById("input-tweet").value.length > 0) {
+    document.getElementById("button-tweet").disabled = false;
+    } else {
+    document.getElementById("button-tweet").disabled = true;
+    }
+  }
+});
+
+function printTweet(lastInput) {
+  let listItem = document.createElement("li");
+  let tweetInput = document.createTextNode(lastInput);
+  listItem.appendChild(tweetInput);
+  let ul = document.getElementById("tweets");
+  ul.insertBefore(listItem, ul.firstChild);
+}
